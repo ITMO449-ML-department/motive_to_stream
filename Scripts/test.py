6ba7b0fc8707fda238cc7249cc7601f425310541
Your Name <you@example.com>
2022-11-16 17:29:56 +0300

    traction of one object relatively to another

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,12 +1,8 @@
 import natnetclient as natnet
 import numpy as np
 client = natnet.NatClient(client_ip='127.0.0.1', data_port=1511, comm_port=1510)
-print(client.ping())
 
 while(True):
-    ms = client.unidentified_markers
-    if len(ms) > 0:
-        markers = np.zeros((len(ms),3))
-        for i in range(len(ms)):
-            markers[i] = np.array(ms[i].position[:3])
-        print(markers)
+    center = np.array(client.rigid_bodies["RigidBody_1"].position)
+    position = np.array(client.rigid_bodies["RigidBody_2"].position) - center
+    print(position)
